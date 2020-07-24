ed1b659f9e454961f45709ca15741535b6f77969
unknown <dineshiie@gmail.com>
2020-07-24 21:33:32 +0530

    image upload

---
+++ ufarm_nestjs/src/shared/utils/file-upload.util.ts
@@ -0,0 +1,6 @@
+export const imageFileFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(new Error('Only image files are supported!'), false);
+  }
+  cb(null, true);
+};
